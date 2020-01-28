568beff4375b817d7e37c22702deb7ce26fafd41
Queen Shabazz <queennefertitishabazz@gmail.com>
2020-01-27 23:14:35 -0800

    Updates

---
+++ precache-manifest.10868d6f39e427b6f7bdb20db0ef265a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0bb40b2ae6419d1962dee07e465d64c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0cc2fe73dc20df633d17",
+    "url": "/static/css/main.99764550.chunk.css"
+  },
+  {
+    "revision": "6fddd2f2e03716763324",
+    "url": "/static/js/2.5a76e7d5.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.5a76e7d5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0cc2fe73dc20df633d17",
+    "url": "/static/js/main.40afbe92.chunk.js"
+  },
+  {
+    "revision": "04c4776ccd74bbaec0a1",
+    "url": "/static/js/runtime-main.91a1eff3.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/gumbo-app/precache-manifest.6a6bf376b276e369dae872e37fbdb26e.js"
+  "/precache-manifest.10868d6f39e427b6f7bdb20db0ef265a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/gumbo-app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
