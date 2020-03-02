0d89636e199f278cf508e541c93151446a5efc82
Queen Shabazz <queennefertitishabazz@gmail.com>
2020-03-02 15:57:22 -0800

    screenshots

---
+++ build/precache-manifest.9bd88cb0ca945352fc240b3a4cea8774.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "435cbf72ece2b9114894c171d7965ef1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f819f9acdef910d8d89d",
+    "url": "/static/css/main.2f063412.chunk.css"
+  },
+  {
+    "revision": "543e69a4d94785067937",
+    "url": "/static/js/2.ec59e220.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.ec59e220.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f819f9acdef910d8d89d",
+    "url": "/static/js/main.3b6b274c.chunk.js"
+  },
+  {
+    "revision": "04c4776ccd74bbaec0a1",
+    "url": "/static/js/runtime-main.91a1eff3.js"
+  }
+]);
---
+++ src/NewRecipe/NewRecipe.js
@@ -0,0 +1,95 @@
+import React, {Component} from 'react';
+import config from '../config'
+
+class NewRecipe extends Component{
+        constructor(props){
+            super(props)
+            this.state = {
+                title:'',
+                recipe:'',
+                error:''
+            }
+            this.handleChange=this.handleChange.bind(this)
+            this.newRecipe= this.newRecipe.bind(this)
+
+        }
+        handleChange = (e) =>{
+            this.setState({
+                [e.target.name]: e.target.value
+            })
+        }
+        newRecipe(e){
+        e.preventDefault()
+        fetch(`${config.API_ENDPOINT}/recipes`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Access-Control-Allow-Origin": "*"
+                // "Content-Type": "text/plain"
+              },
+            body: JSON.stringify({
+                title: this.state.title,
+                ingredients: this.state.recipe,
+             })
+        })
+        .then(res=>{
+            if(!res.ok){
+                throw res
+            }
+            return res.json();
+        })
+        .then(data => { 
+            window.location=window.location.origin + '/saved'
+
+         })
+         .catch(err => {
+            if(err.status===400){
+                this.setState({error: "recipe not saved"})
+            }
+            })
+           }
+        render() {
+                return (
+               <>
+               <form> 
+                    <label htmlFor="#text-area"> Title: </label>
+                    <input id="text-area" type="text" name="title" value={this.state.title} onChange={this.handleChange}/>
+                    <label htmlFor="#recipe-area"> Recipe: </label>
+                    <input id="recipe-area" type="text" name="recipe" value={this.state.recipe} onChange={this.handleChange}/>
+                    <button type="submit" className="regular-button" onClick={(e)=>{this.newRecipe(e)}}> Submit</button>
+
+                </form> 
+               
+                    {/* <section id="intro">
+                        <h2> Edit Recipe: {editItem[0].title}
+                        </h2>
+                        <p> See below for saved searches, or find the dish nearby
+                        </p>
+                    </section>
+                    <div>
+                       <form>
+                          <label htmlFor="recipe-title">
+                              Recipe Title
+                            <input type='text' placeholder={editItem[0].title} value={this.props.editRecipeTitle} onChange={this.props.onChangeRecipeTitle}/>
+                            </label> 
+                            <label > Ingredients
+                            <input type='text-area' placeholder={editItem[0].ingredients} value={this.props.editRecipeIngredients} onChange={this.props.onChangeRecipeIngredients}/>
+                           
+                            </label> 
+                            <button type="submit" className="regular-button" onClick={(e)=>{this.props.updateRecipe(editId, e)}}> Submit</button>
+
+                       </form>
+                    </div>
+                    </>
+                    ) : (
+                        <div>Loading</div>
+                    )
+                
+                } */}
+                </>
+            )
+        }
+        
+}
+
+export default NewRecipe
