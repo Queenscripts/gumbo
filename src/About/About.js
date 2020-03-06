7ce7c309c5178a1f8bc50894f062a4fc019383da
queennefertitishabazz <queennefertitishabazz@gmail.com>
2020-03-05 22:18:06 -0800

    navbar desktop styling

--- src/About/About.js
+++ src/About/About.js
@@ -8,7 +8,7 @@ export default class About extends Component{
                 
                 <section id="howto">
                     <h2> How It Works</h2>
-                    <p> Search  favorite ingredient of yours to find recipes that use that ingredient</p>
+                    <p> Search  favorite ingredient of yours to find recipes that use that ingredient.</p>
                 </section>
                 <section id="screenshot">
                     <h2> Try It Out</h2>
