7bba670e9b2429c9496791979e541b2b1f533768
Queen <queennefertitishabazz@gmail.com>
2020-03-08 23:54:27 -0700

    fetch import

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import Recipes from './Recipes/Recipes';
 import EditRecipe from './EditRecipe/EditRecipe'
 import config from './config'
 import NewRecipe from './NewRecipe/NewRecipe';
+import fetch from 'isomorphic-fetch'
 
 
 class App extends Component{
