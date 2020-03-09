11f4c906b4f75dac03ed8248c542e7994c1fe8b1
Queen <queennefertitishabazz@gmail.com>
2020-03-09 00:03:35 -0700

    'isomorphic-fetch';

--- src/App.js
+++ src/App.js
@@ -10,7 +10,8 @@ import Recipes from './Recipes/Recipes';
 import EditRecipe from './EditRecipe/EditRecipe'
 import config from './config'
 import NewRecipe from './NewRecipe/NewRecipe';
-import fetch from 'isomorphic-fetch'
+import fetch from 'isomorphic-fetch';
+
 
 
 class App extends Component{
