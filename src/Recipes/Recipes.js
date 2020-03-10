4ae17c4be7c2683b65cefcf71d4af25e90b1241f
Queen <queennefertitishabazz@gmail.com>
2020-03-10 02:36:14 -0700

    Update Recipes.js

--- src/Recipes/Recipes.js
+++ src/Recipes/Recipes.js
@@ -67,7 +67,7 @@ class Recipes extends Component {
                     </td>
                     <td>
            
-                    <button onClick={(e)=>{this.props.addRecipe(e); this.props}}>
+                    <button onClick={(e)=>{this.props.addRecipe(e); console.log(this.props)}}>
                              Add recipe to personal cookbook
                     </button>
                     </td>
