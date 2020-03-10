43dc7afed1ac9889984d650ca88ad5dd1e286b50
Queen <queennefertitishabazz@gmail.com>
2020-03-09 22:46:21 -0700

    Update Recipes.js

--- src/Recipes/Recipes.js
+++ src/Recipes/Recipes.js
@@ -1,7 +1,6 @@
 import React, {Component} from 'react'
 import config from '../config'
 import './Recipes.css'
-import SavedSearches from './SavedSearches/SavedSearches';
 
 class Recipes extends Component {
     constructor(){
