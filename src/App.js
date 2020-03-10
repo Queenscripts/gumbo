290f85140d070329e251ea803f8c42bbd82ddf0f
Queen <queennefertitishabazz@gmail.com>
2020-03-10 03:35:09 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -107,7 +107,7 @@ class App extends Component{
         })
         .then(data=>(
             alert(data.title + " saved!"),
-            this.setState({savedRecipes:data})
+            this.setState({savedRecipes:this.state.savedRecipes.concat(data)})
           
         ))
         .catch(
