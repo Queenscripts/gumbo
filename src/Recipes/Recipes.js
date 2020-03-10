7002bef90ff2d2e73f40511880379e52e525c710
Queen <queennefertitishabazz@gmail.com>
2020-03-09 23:01:31 -0700

    import on SS component

--- src/Recipes/Recipes.js
+++ src/Recipes/Recipes.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react'
 import config from '../config'
 import './Recipes.css'
+import SavedSearches from './SavedSearches/SavedSearches'
 
 class Recipes extends Component {
     constructor(){
@@ -40,7 +41,7 @@ class Recipes extends Component {
         })
         .then(data=>(
             alert(data.title + " saved!"),
-            console.log('data', data)
+           <SavedSearches data={data}/>
            
         ))
         .catch(
