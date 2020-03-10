f14e1571de5cd9f11251cb6b8e19919b784428c0
Queen <queennefertitishabazz@gmail.com>
2020-03-10 00:48:37 -0700

    Update SavedSearches.js

--- src/SavedSearches/SavedSearches.js
+++ src/SavedSearches/SavedSearches.js
@@ -14,6 +14,7 @@ class SavedSearches extends Component{
         )
                 :
                 (
+                    {    window.location.reload(true);}
                 <div>
                 <section id="intro">
                 <header> Your Saved Searches & Recipes </header>
